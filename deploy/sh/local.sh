46d69a4c4c017f7dc9a383547dab0f4a3f6344ab
marouane <zaag.marouane@gmail.com>
2023-12-13 20:02:33 +0100

    k8s working

--- deploy/sh/local.sh
+++ deploy/sh/local.sh
@@ -3,33 +3,20 @@
 # For Local environmnet
 
 echo "Creating Spring Boot demo application for Kubernetes on local environment"
+minikube stop
+minikube start
+
+kubectl apply -f ../db/postgres-configmap.yml
+kubectl apply -f ../db/postgres-credentials.yml
 
 ### Create deployments and services
 # db
-kubectl apply -f ../db/statefulset.yaml
-kubectl apply -f ../db/service.yaml
+kubectl apply -f ../db/deployment.yml
 
 # apserver (For Local environment)
-kubectl apply -f ../app/deployment.yaml
-kubectl apply -f ../app/service.yaml
-
-# LB
-#kubectl apply -f ../lb/deployment.yaml
-#kubectl apply -f ../lb/loadbalancer.yaml
+kubectl apply -f ../app/deployment.yml
 
 CTX=$(kubectl config current-context)
 echo ${CTX}
 
-if [ "${CTX}" == "minikube" ]; then
-    while true; do
-        URL=$(minikube service my-app --url)
-        echo ${URL}
-        if [ "${URL}" == "" ]; then
-            sleep 1
-        else
-            break;
-        fi
-    done
-else
-    URL="http://localhost"
-fi
+minikube service my-app
